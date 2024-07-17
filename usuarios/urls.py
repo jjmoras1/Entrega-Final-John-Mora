e78ae98dc540603578fa7d95730e66cc8443267c
JohnM <jjmoras1@gmail.com>
2024-07-17 00:46:47 -0500

    Registro de Usuario

---
+++ usuarios/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from usuarios import views
+urlpatterns=[
+    path('logout/',LogoutView.as_view(template_name="usuarios/cierre_de_sesion.html"),name="logout"),
+    path('login/',views.login,name="login"),
+    path('registro/',views.registro,name="registro")
+]
